067997cad76618aec4cda12baf140251f7b27b01
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-10-17 19:49:33 -0500

    Added slide-in animations for changing customizations

--- Grombdoll/ViewModels/DressUpViewModel.cs
+++ Grombdoll/ViewModels/DressUpViewModel.cs
@@ -6,6 +6,16 @@ namespace Grombdoll.ViewModels {
         public Func<bool> ShowGalleryView;
         public Func<bool> ShowCreditsView;
 
+        public Action OnCustomizationChanged;
+        public Action OnBaseChanged;
+        public Action OnEyesChanged;
+        public Action OnMouthChanged;
+        public Action OnOutfitChanged;
+        public Action OnShoesChanged;
+        public Action OnAccessoryChanged;
+        public Action OnBackgroundChanged;
+        public Action OnReset;
+
         private int curBase = 0;
         public string CurBaseImagePath { get { return baseImagePaths[curBase]; } }
         private string[] baseImagePaths = {
@@ -17,6 +27,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementBaseSelection() {
             curBase = (curBase + 1) % baseImagePaths.Length;
             OnPropertyChanged(nameof(CurBaseImagePath));
+
+            OnBaseChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curEyes = 0;
@@ -30,6 +43,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementEyesSelection() {
             curEyes = (curEyes + 1) % eyesImagePaths.Length;
             OnPropertyChanged(nameof(CurEyesImagePath));
+
+            OnEyesChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curMouth = 0;
@@ -45,6 +61,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementMouthSelection() {
             curMouth = (curMouth + 1) % mouthImagePaths.Length;
             OnPropertyChanged(nameof(CurMouthImagePath));
+
+            OnMouthChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curOutfit = 0;
@@ -63,6 +82,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementOutfitSelection() {
             curOutfit = (curOutfit + 1) % outfitImagePaths.Length;
             OnPropertyChanged(nameof(CurOutfitImagePath));
+
+            OnOutfitChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curShoes = 0;
@@ -76,6 +98,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementShoesSelection() {
             curShoes = (curShoes + 1) % shoesImagePaths.Length;
             OnPropertyChanged(nameof(CurShoesImagePath));
+
+            OnShoesChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curAccessory = 0;
@@ -92,6 +117,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementAccessorySelection() {
             curAccessory = (curAccessory + 1) % accessoryImagePaths.Length;
             OnPropertyChanged(nameof(CurAccessoryImagePath));
+
+            OnAccessoryChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         private int curBackground = 0;
@@ -106,6 +134,9 @@ namespace Grombdoll.ViewModels {
         public void IncrementBackgroundSelection() {
             curBackground = (curBackground + 1) % backgroundImagePaths.Length;
             OnPropertyChanged(nameof(CurBackgroundImagePath));
+
+            OnBackgroundChanged?.Invoke();
+            OnCustomizationChanged?.Invoke();
         }
 
         public void ResetCustomizations() {
@@ -129,6 +160,8 @@ namespace Grombdoll.ViewModels {
 
             curBackground = 0;
             OnPropertyChanged(nameof(CurBackgroundImagePath));
+
+            OnReset?.Invoke();
         }
 
         public DressUpViewModel(Func<bool> showTitleView, Func<bool> showGalleryView, Func<bool> showCreditsView) {
--- Grombdoll/Views/DressUpView.xaml.cs
+++ Grombdoll/Views/DressUpView.xaml.cs
@@ -1,52 +1,68 @@
 ﻿using Grombdoll.ViewModels;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Grombdoll.Views {
     public partial class DressUpView : UserControl {
         DressUpViewModel _dressUpViewModel;
+
+        private float slideInTime = 0.3f;
+        private Duration slideInDuration;
+        private DoubleAnimation slideInAnimation;
+        private IEasingFunction slideInEasingFunction;
+
         public DressUpView() {
             Loaded += OnLoaded;
 
             InitializeComponent();
         }
-        private void OnLoaded(object sender, RoutedEventArgs e) { _dressUpViewModel = (DressUpViewModel)DataContext; }
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            _dressUpViewModel = (DressUpViewModel)DataContext;
 
-        private void BaseButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementBaseSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void EyesButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementEyesSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void MouthButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementMouthSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void OutfitButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementOutfitSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void ShoesButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementShoesSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void AccessoryButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementAccessorySelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void BackgroundButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.IncrementBackgroundSelection();
-            PlayAccessoryIncrementSFX();
-        }
-        private void ResetButtonClicked(object sender, RoutedEventArgs e) {
-            _dressUpViewModel.ResetCustomizations();
-            PlayAccessoryIncrementSFX();
+            slideInDuration = new Duration(TimeSpan.FromSeconds(slideInTime));
+            slideInAnimation = new DoubleAnimation(100, 0, slideInDuration);
+            slideInEasingFunction = new BackEase() { EasingMode = EasingMode.EaseOut };
+            slideInAnimation.EasingFunction = slideInEasingFunction;
+
+            _dressUpViewModel.OnCustomizationChanged += PlayCustomizationChangedSound;
+            _dressUpViewModel.OnBaseChanged += PlayBaseSlideInAnimation;
+            _dressUpViewModel.OnEyesChanged += PlayEyesSlideInAnimation;
+            _dressUpViewModel.OnMouthChanged += PlayMouthSlideInAnimation;
+            _dressUpViewModel.OnOutfitChanged += PlayOutfitSlideInAnimation;
+            _dressUpViewModel.OnShoesChanged += PlayShoesSlideInAnimation;
+            _dressUpViewModel.OnAccessoryChanged += PlayAccessorySlideInAnimation;
+            _dressUpViewModel.OnBackgroundChanged += PlayBackgroundSlideInAnimation;
+            _dressUpViewModel.OnReset += PlayResetAnimation;
         }
 
-        private void PlayAccessoryIncrementSFX() {
+        private void BaseButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementBaseSelection();
+        private void EyesButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementEyesSelection();
+        private void MouthButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementMouthSelection();
+        private void OutfitButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementOutfitSelection();
+        private void ShoesButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementShoesSelection();
+        private void AccessoryButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementAccessorySelection();
+        private void BackgroundButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.IncrementBackgroundSelection();
+        private void ResetButtonClicked(object sender, RoutedEventArgs e) => _dressUpViewModel.ResetCustomizations();
 
+
+        private void PlayBaseSlideInAnimation() => Base.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayEyesSlideInAnimation() => Eyes.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayMouthSlideInAnimation() => Mouth.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayOutfitSlideInAnimation() => Outfit.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayShoesSlideInAnimation() => Shoes.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayAccessorySlideInAnimation() => Accessory.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+        private void PlayBackgroundSlideInAnimation() => Background.RenderTransform.BeginAnimation(TranslateTransform.XProperty, slideInAnimation);
+
+        private void PlayResetAnimation() {
+            DoubleAnimation scaleAnimation = new DoubleAnimation(1.1, 1, slideInDuration);
+            scaleAnimation.EasingFunction = new QuadraticEase() { EasingMode = EasingMode.EaseOut };
+            AllLayers.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            AllLayers.RenderTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
         }
+
+        private void PlayCustomizationChangedSound() { }
     }
 }
